c5259f127e1723f95f3936b22f502c42c84f833d
melroyd16 <m_dmello@live.com>
2017-03-25 11:54:02 -0700

    temporary code to populate data. TO BE Removed later

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -5,11 +5,29 @@
         .module('UnistatsApp.Home', [])
         .controller('HomeCtrl', HomeCtrl);
 
-    HomeCtrl.$inject = [];
+    HomeCtrl.$inject = ['$http'];
 
     /* @ngInject */
-    function HomeCtrl() {
+    function HomeCtrl($http) {
         var vm = this;
         vm.message = 'Hello from home page';
+        vm.populateData = populateData;
+
+        function populateData() {
+            $http.get('JSON/data.json').then(function (data) {
+                for (var i = 1801; i < data.data.length; i++) {
+                    var payload = {
+                        operation: "populateData",
+                        populatePayload: {
+                            TableName: "university",
+                            Item: data.data[i]
+                        }
+                    }
+                    $http.post(UNIVERSITY_DETAILS_API, payload).then(function (data) {
+                        return data;
+                    })
+                }
+            })
+        }
     }
 })();
